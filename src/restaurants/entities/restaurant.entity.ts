3b366b13f27b4126adbb05a9f39ba6ad8f25276e
ktw3577 <taewongim06@gmail.com>
2021-07-16 23:36:15 +0900

    connect Entity to existing entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,22 +1,36 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((type) => String)
+  @Column()
   @IsString()
   @Length(5, 10)
   name: string;
 
   @Field((type) => Boolean)
+  @Column()
   @IsBoolean()
   isVegan: boolean;
 
   @Field((type) => String)
+  @Column()
   @IsString()
   address: string;
 
   @Field((type) => String)
+  @Column()
   @IsString()
   ownersName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
