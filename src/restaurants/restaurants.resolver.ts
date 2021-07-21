01bb80ca90a4fd0be66af350d4eb90d08a409179
ktw3577 <taewongim06@gmail.com>
2021-07-21 21:34:33 +0900

    add mutation

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -11,6 +12,7 @@ export class RestaurantResolver {
   restaurants(): Promise<Restaurant[]> {
     return this.restaurantService.getAll();
   }
+
   @Mutation((returns) => Boolean)
   async createRestaurant(
     @Args('input') createRestaurantDto: CreateRestaurantDto,
@@ -22,5 +24,12 @@ export class RestaurantResolver {
       console.log(e);
       return false;
     }
+  }
+
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
+    return true;
   }
 }
