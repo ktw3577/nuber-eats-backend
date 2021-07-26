a1a2c4614d53d8af81d4f6a86702115b6bb5a883
ktw3577 <taewongim06@gmail.com>
2021-07-26 21:47:00 +0900

    create mutation & dto

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,18 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column } from 'typeorm';
 
 type UserRole = 'client' | 'owner' | 'delivery';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 export class User extends CoreEntity {
   @Column()
+  @Field((type) => String)
   email: string;
 
   @Column()
+  @Field((type) => String)
   password: string;
 
   @Column()
