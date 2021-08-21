3c3fffcb2c52f02c6e17b4b50d705fdb6a214d85
ktw3577 <taewongim06@gmail.com>
2021-08-21 21:22:31 +0900

    create verifications

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UserService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Verification])],
   providers: [],
   exports: [UserService],
 })
